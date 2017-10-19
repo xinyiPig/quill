4ca9703699a7f44ecf1c10b1506fe3a910bafcbd
xinyiPig <30715127+xinyiPig@users.noreply.github.com>
2017-10-19 19:14:01 +0800

    Update size.js

--- formats/size.js
+++ formats/size.js
@@ -2,11 +2,11 @@ import Parchment from 'parchment';
 
 let SizeClass = new Parchment.Attributor.Class('size', 'ql-size', {
   scope: Parchment.Scope.INLINE,
-  whitelist: ['small', 'large', 'huge']
+  whitelist: ['12px','13px','14px','15px','16px','18px','20px','22px','24px','26px']
 });
 let SizeStyle = new Parchment.Attributor.Style('size', 'font-size', {
   scope: Parchment.Scope.INLINE,
-  whitelist: ['10px', '18px', '32px']
+  whitelist: ['12px','13px','14px','15px','16px','18px','20px','22px','24px','26px']
 });
 
 export { SizeClass, SizeStyle };
