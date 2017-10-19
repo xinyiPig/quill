0d597aaaea658d42aec1a9b750eb2d8885ee887a
rookielzy <slimjetlzy@gmail.com>
2017-10-19 22:42:34 +0800

    commit dist

--- formats/size.js
+++ formats/size.js
@@ -2,11 +2,11 @@ import Parchment from 'parchment';
 
 let SizeClass = new Parchment.Attributor.Class('size', 'ql-size', {
   scope: Parchment.Scope.INLINE,
-  whitelist: ['12px','13px','14px','15px','16px','18px','20px','22px','24px','26px']
+  whitelist: ['12px', '13px', '14px', '15px', '16px', '18px', '20px', '22px', '24px', '26px']
 });
 let SizeStyle = new Parchment.Attributor.Style('size', 'font-size', {
   scope: Parchment.Scope.INLINE,
-  whitelist: ['12px','13px','14px','15px','16px','18px','20px','22px','24px','26px']
+  whitelist: ['12px', '13px', '14px', '15px', '16px', '18px', '20px', '22px', '24px', '26px']
 });
 
 export { SizeClass, SizeStyle };
